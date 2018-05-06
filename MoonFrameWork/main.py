688ab969fd17edd1fc8bc41d48a1850a3e35a828
Unknown <jjjooonnn@gmail.com>
2018-05-06 23:34:28 +0200

    fixes

--- MoonFrameWork/main.py
+++ MoonFrameWork/main.py
@@ -7,14 +7,15 @@ outFile = open('result', 'w')
 
 # p = subprocess.Popen(['/home/borowski/Moongen/moongen-simple', 'start',
 #                       'udp-simple:0:1:rate=1000mbit/s,ratePattern=poisson'], stdout=outFile)
-p = subprocess.Popen(['./../../../MoonGen/moongen-simple', 'start', 'udp-simple:0:1:rate=1000mbit/s,ratePattern=poisson'],
-                     stdout=outFile)
+p = subprocess.Popen(
+    ['./../../../MoonGen/moongen-simple', 'start', 'udp-simple:0:1:rate=1000mbit/s,ratePattern=poisson'],
+    stdout=outFile)
 
 result = subprocess.Popen.wait(p)
 
 if result == 0:
     print('test terminated, everything worked out.')
-else
+else:
     print('exit code: Something went wrong!')
 
 # class TestMoonGenSimple(unittest.TestCase):
