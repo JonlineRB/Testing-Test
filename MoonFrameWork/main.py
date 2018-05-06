a1c2d2dec6a08e39f0f26cd44cc7f60b9d70a162
Unknown <jjjooonnn@gmail.com>
2018-05-06 17:23:08 +0200

    testing echo

--- MoonFrameWork/main.py
+++ MoonFrameWork/main.py
@@ -4,6 +4,12 @@ print ("Testing MoonGen Simple")
 
 # go to MoonGen repo and execute a simple flow
 
-moonGenPath = "~/MoonGen/"
-simpleFlow = "moongen-simple start load-latency:0:1:rate=10Mp/s,time=3m"
-call(["./../../../MoonGen/moongen-simple start load-latency:0:1:rate=10Mp/s,time=3m"])
+def callSimple1:
+    # moonGenPath = "~/MoonGen/"
+    # simpleFlow = "moongen-simple start load-latency:0:1:rate=10Mp/s,time=3m"
+    call(["./../../../MoonGen/moongen-simple start load-latency:0:1:rate=10Mp/s,time=3m"])
+
+def echoTest:
+    call(["echo Test"])
+
+echoTest()
