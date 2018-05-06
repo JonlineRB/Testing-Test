209517e2f0157376a84c770f554371834609ef80
Unknown <jjjooonnn@gmail.com>
2018-05-06 21:24:00 +0200

    first test case

--- MoonFrameWork/main.py
+++ MoonFrameWork/main.py
@@ -1,20 +1,30 @@
-from subprocess import call
+import subprocess
+import unittest
 
 print ("Testing MoonGen Simple")
 
 
+class TestMoonGenSimple(unittest.TestCase):
+
+    def testSimpleUdp():
+        # use a file for output
+        outFile = open('result', 'w')
+        # execute MoonGenSimple: udp-simple
+        # p = self.subprocess.Popen([])
+
+
 # go to MoonGen repo and execute a simple flow
 
-def callsimple1():
-    # moonGenPath = "~/MoonGen/"
-    # simpleFlow = "moongen-simple start load-latency:0:1:rate=10Mp/s,time=3m"
-    call("./../../../MoonGen/moongen-simple start load-latency:0:1:rate=10Mp/s,time=3m",shell=True)
+# def callsimple1():
+#     # moonGenPath = "~/MoonGen/"
+#     # simpleFlow = "moongen-simple start load-latency:0:1:rate=10Mp/s,time=3m"
+#     call("./../../../MoonGen/moongen-simple start load-latency:0:1:rate=10Mp/s,time=3m",shell=True)
 
-def callsimplelocal():
-    call("./moongen-simple start load-latency:0:1:rate=10Mp/s,time=3m",shell=True)
+# def callsimplelocal():
+#     call("./moongen-simple start load-latency:0:1:rate=10Mp/s,time=3m",shell=True)
 
-def echotest():
-    call("echo TEST",shell=True)
+# def echotest():
+#     call("echo TEST",shell=True)
 
 
-callsimplelocal()
+TestMoonGenSimple.testSimpleUdp()
