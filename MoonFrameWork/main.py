cb56e46c30fee72c0d290c69cc3d538d4eb83f94
Unknown <jjjooonnn@gmail.com>
2018-05-06 17:51:11 +0200

    removed sudo from the arg

--- MoonFrameWork/main.py
+++ MoonFrameWork/main.py
@@ -8,7 +8,7 @@ print ("Testing MoonGen Simple")
 def callsimple1():
     # moonGenPath = "~/MoonGen/"
     # simpleFlow = "moongen-simple start load-latency:0:1:rate=10Mp/s,time=3m"
-    call("sudo ./../../../MoonGen/moongen-simple start load-latency:0:1:rate=10Mp/s,time=3m",shell=True)
+    call("./../../../MoonGen/moongen-simple start load-latency:0:1:rate=10Mp/s,time=3m",shell=True)
 
 
 def echotest():
