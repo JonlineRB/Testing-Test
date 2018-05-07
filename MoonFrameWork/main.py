d41513793b38be8262b01fa0f0423c789ac77372
Unknown <jjjooonnn@gmail.com>
2018-05-07 09:27:17 +0200

    expressive error message

--- MoonFrameWork/main.py
+++ MoonFrameWork/main.py
@@ -27,7 +27,7 @@ else:
     # parse file for known reasons
     errorFile = open('result', 'r')
     if 'Found 0 usable devices' in errorFile.read():
-        print('No devices available')
+        print('No were devices available. Suggestion: Try running as su, or binding devices.')
     errorFile.close()
 
 print('Framework: end')
