acc8d187d2853f8a220640e8782924efe5d99b1e
Unknown <jjjooonnn@gmail.com>
2018-05-07 09:26:12 +0200

    fixed file handling

--- MoonFrameWork/main.py
+++ MoonFrameWork/main.py
@@ -18,14 +18,16 @@ p = subprocess.Popen(
 #     print(e)
 
 result = subprocess.Popen.wait(p)
+outFile.close()
 
 if result == 0:
     print('==TestFramework: test terminated, everything works.')
 else:
     print('==TestFramework: exit code %s: Something went wrong!' % result)
     # parse file for known reasons
-    if 'Found 0 usable devices' in outFile.read():
+    errorFile = open('result', 'r')
+    if 'Found 0 usable devices' in errorFile.read():
         print('No devices available')
+    errorFile.close()
 
-outFile.close()
 print('Framework: end')
