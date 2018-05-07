d76725101d782207da2df97137d17b5c700bd5f0
Unknown <jjjooonnn@gmail.com>
2018-05-07 09:22:39 +0200

    parsing out file for 0 usable devices

--- MoonFrameWork/main.py
+++ MoonFrameWork/main.py
@@ -1,30 +1,31 @@
 # import unittest
 import subprocess
 
+print('Framework: start')
 print ("Testing MoonGen Simple Case: udp-simple")
 
-# outFile = open('result', 'w')
+outFile = open('result', 'w')
 
-# p = subprocess.Popen(['/home/borowski/Moongen/moongen-simple', 'start',
-#                       'udp-simple:0:1:rate=1000mbit/s,ratePattern=poisson'], stdout=outFile)
+p = subprocess.Popen(
+    ['./../../../MoonGen/moongen-simple', 'start', 'udp-simple:0:1:rate=1000mbit/s,ratePattern=poisson'],
+    stdout=outFile)
 
+# try:
+#     p = subprocess.check_output(
+#         ['./../../../MoonGen/moongen-simple', 'start', 'udp-simple:0:1:rate=1000mbit/s,ratePattern=poisson'])
+#     print("=== No Errors===")
+# except subprocess.CalledProcessError as e:
+#     print(e)
 
-# p = subprocess.Popen(
-#     ['./../../../MoonGen/moongen-simple', 'start', 'udp-simple:0:1:rate=1000mbit/s,ratePattern=poisson'],
-#     stdout=outFile)
+result = subprocess.Popen.wait(p)
 
-try:
-    p = subprocess.check_output(['./../../../MoonGen/moongen-simple', 'start', 'udp-simple:0:1:rate=1000mbit/s,ratePattern=poisson'])
-    print("=== No Errors===")
-except subprocess.CalledProcessError as e:
-    print(e)
+if result == 0:
+    print('==TestFramework: test terminated, everything works.')
+else:
+    print('==TestFramework: exit code %s: Something went wrong!' % result)
+    # parse file for known reasons
+    if 'Found 0 usable devices' in outFile.read():
+        print('No devices available')
 
-# result = subprocess.Popen.wait(p)
-
-# if result == 0:
-#     print('==TestFramework: test terminated, everything works.')
-# else:
-#     print('==TestFramework: exit code %s: Something went wrong!' % result)
-    # parse file
-
-# outFile.close()
+outFile.close()
+print('Framework: end')
