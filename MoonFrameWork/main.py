bee0cc1c01a0d7ff8fd0b043a0c8f59f4671c798
Unknown <jjjooonnn@gmail.com>
2018-05-07 17:50:47 +0200

    forgot a slash

--- MoonFrameWork/main.py
+++ MoonFrameWork/main.py
@@ -8,7 +8,7 @@ outFile = open('result', 'w')
 
 p = subprocess.Popen(
     ['./../../../MoonGen/moongen-simple', 'start', 'udp-simple:0:1:rate=1000mbit/s,ratePattern=poisson'],
-    stdout=outFile, cwd='home/borowski/MoonGen/')
+    stdout=outFile, cwd='/home/borowski/MoonGen/')
 
 # try:
 #     p = subprocess.check_output(
