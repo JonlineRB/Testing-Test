c8039d25bf084c5b85ea915575021621a9af8365
Unknown <jjjooonnn@gmail.com>
2018-05-07 09:05:34 +0200

    changed to check_output

--- MoonFrameWork/main.py
+++ MoonFrameWork/main.py
@@ -3,42 +3,28 @@ import subprocess
 
 print ("Testing MoonGen Simple Case: udp-simple")
 
-outFile = open('result', 'w')
+# outFile = open('result', 'w')
 
 # p = subprocess.Popen(['/home/borowski/Moongen/moongen-simple', 'start',
 #                       'udp-simple:0:1:rate=1000mbit/s,ratePattern=poisson'], stdout=outFile)
-p = subprocess.Popen(
-    ['./../../../MoonGen/moongen-simple', 'start', 'udp-simple:0:1:rate=1000mbit/s,ratePattern=poisson'],
-    stdout=outFile)
 
-result = subprocess.Popen.wait(p)
+
+# p = subprocess.Popen(
+#     ['./../../../MoonGen/moongen-simple', 'start', 'udp-simple:0:1:rate=1000mbit/s,ratePattern=poisson'],
+#     stdout=outFile)
+
+try:
+    p = subprocess.check_output(['./../../../MoonGen/moongen-simple', 'start', 'udp-simple:0:1:rate=1000mbit/s,ratePattern=poisson'])
+    print("=== No Errors===")
+except subprocess.CalledProcessError as e:
+    print(e)
+
+# result = subprocess.Popen.wait(p)
 
 if result == 0:
-    print('==TestFramework: test terminated, everything worked out.')
+    print('==TestFramework: test terminated, everything works.')
 else:
     print('==TestFramework: exit code %s: Something went wrong!' % result)
     # parse file
 
-outFile.close()
-
-# class TestMoonGenSimple(unittest.TestCase):
-
-# def testSimpleUdp(self):
-# use a file for output
-# outFile = open('result', 'w')
-# execute MoonGenSimple: udp-simple
-# p = self.subprocess.Popen([])
-
-
-# go to MoonGen repo and execute a simple flow
-
-# def callsimple1():
-#     # moonGenPath = "~/MoonGen/"
-#     # simpleFlow = "moongen-simple start load-latency:0:1:rate=10Mp/s,time=3m"
-#     call("./../../../MoonGen/moongen-simple start load-latency:0:1:rate=10Mp/s,time=3m",shell=True)
-
-# def callsimplelocal():
-#     call("./moongen-simple start load-latency:0:1:rate=10Mp/s,time=3m",shell=True)
-
-# def echotest():
-#     call("echo TEST",shell=True)
+# outFile.close()
