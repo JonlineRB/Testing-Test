5d9c7110e149712bd3182ba5a92d056b00dd169b
Unknown <jjjooonnn@gmail.com>
2018-06-17 17:57:22 +0200

    fix

--- MoonFrameWork/tests.py
+++ MoonFrameWork/tests.py
@@ -636,7 +636,7 @@ class TestL3TcpSynFlood(TerminatingTest):
         vallist[self.valueindex['tx1avg']] /= avgcounter
         self.summarylog.write('DEVICE 1 TX values:\nMAX' + str(vallist[self.valueindex['tx1max']]) + '\nMIN: ' + str(
             vallist[self.valueindex['tx1min']]) + '\nAVG: ' + str(vallist[self.valueindex['tx1avg']]) + '\n')
-        if len(self.devicelist == 2):
+        if len(self.devicelist) == 2:
             self.summarylog.write(
                 'DEVICE 2 TX values:\nMAX' + str(vallist[self.valueindex['tx2max']]) + '\nMIN: ' + str(
                     vallist[self.valueindex['tx2min']]) + '\nAVG: ' + str(vallist[self.valueindex['tx2avg']]) + '\n')
