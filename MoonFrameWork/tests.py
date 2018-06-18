df2631de77939ba3f903e4ec92935b539dd310b3
Unknown <jjjooonnn@gmail.com>
2018-06-18 21:59:30 +0200

    try

--- MoonFrameWork/tests.py
+++ MoonFrameWork/tests.py
@@ -692,7 +692,7 @@ class TestQualityOfService(TerminatingTest):
                 value = float(lines[i].split()[3])
                 # print firstport
                 # print ('THIS HAPPENS. Value: ' + value)
-                if firstport in lines[i]:
+                if firstport + ']' in lines[i]:
                     print 'THIS HAPPENS'
                     if 'TX' in lines[i]:
                         firstporttxvalues = self.adjustvalues(firstporttxvalues, value, firstminmax[0])
