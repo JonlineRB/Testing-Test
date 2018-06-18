34b79fdf68b6e81bca8c17903a7d3e81e6ebde59
Unknown <jjjooonnn@gmail.com>
2018-06-18 23:42:56 +0200

    t

--- MoonFrameWork/tests.py
+++ MoonFrameWork/tests.py
@@ -684,7 +684,8 @@ class TestQualityOfService(TerminatingTest):
         secondport = secondportline[len(secondportline) - 1]
 
         # problem with the port string values, fix it
-        # print('value1: ->' + firstport + '<- || value2: ->' + secondport + '<-')
+        print('value1: ->' + firstport + '<- || value2: ->' + secondport + '<-')
+        print firstport == '42'
 
         firstporttxvalues = [0.0, 0.0, 0.0, 0]
         firstportrxvalues = [0.0, 0.0, 0.0, 0]
@@ -697,7 +698,7 @@ class TestQualityOfService(TerminatingTest):
                 value = float(lines[i].split()[3])
                 # print firstport
                 # print ('THIS HAPPENS. Value: ' + value)
-                if firstportline[len(firstportline) - 1] in lines[i]:
+                if str(firstport) in lines[i]:
                     print 'THIS HAPPENS'
                     if 'TX' in lines[i]:
                         firstporttxvalues = self.adjustvalues(firstporttxvalues, value, firstminmax[0])
