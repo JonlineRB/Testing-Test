355478dd623716c1b66982da96846a7a0d7a4000
Unknown <jjjooonnn@gmail.com>
2018-06-18 19:14:53 +0200

    c

--- MoonFrameWork/tests.py
+++ MoonFrameWork/tests.py
@@ -690,7 +690,7 @@ class TestQualityOfService(TerminatingTest):
             if '[Port' in lines[i]:
                 # print 'THIS HAPPENS'
                 value = float(lines[i].split()[3])
-                # print 'THIS HAPPENS'
+                print firstport
                 # print ('THIS HAPPENS. Value: ' + value)
                 if firstport in lines[i]:
                     if 'TX' in lines[i]:
