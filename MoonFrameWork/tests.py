b8d0b82f2622087e88c6e48f235f06746620a580
Unknown <jjjooonnn@gmail.com>
2018-06-19 17:50:00 +0200

    tweak

--- MoonFrameWork/tests.py
+++ MoonFrameWork/tests.py
@@ -758,7 +758,7 @@ class TestL3TcpSynFlood(DevicesUpCheckedSeperately):
                         continue
 
         vallist[self.valueindex['tx1avg']] /= avgcounter
-        self.summarylog.write('DEVICE 1 TX values:\nMAX' + str(vallist[self.valueindex['tx1max']]) + '\nMIN: ' + str(
+        self.summarylog.write('DEVICE 1 TX values:\nMAX: ' + str(vallist[self.valueindex['tx1max']]) + '\nMIN: ' + str(
             vallist[self.valueindex['tx1min']]) + '\nAVG: ' + str(vallist[self.valueindex['tx1avg']]) + '\n')
         if len(self.devicelist) == 2:
             vallist[self.valueindex['tx2avg']] /= avgcounter
