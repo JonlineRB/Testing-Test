e86994fc9b49d0e103ca2ef46b72c790a776e940
Jonathan <jjjooonnn@gmail.com>
2018-06-21 08:09:03 +0200

    tweak

--- MoonFrameWork/main.py
+++ MoonFrameWork/main.py
@@ -10,12 +10,16 @@ print('Framework: start')
 # case of arg -h
 try:
     if sys.argv[1] == '-h':
-        print 'execute to run the config file.'
-        print 'alternatively, use -t followed by desired tests and device1, device2'
+        print'===================================================================='
+        print 'Welcome to the dedicated testing framework for MoonGen.'
+        print 'execute as is to run the tests according to the config file:'
+        print 'FrameworkConfig.cfg'
+        print 'alternatively, use -t followed by desired tests and device1, device2.'
         print 'you may use PCI express id or device number like in MoonGen'
         print 'either way, make sure absolute path to MoonGen is present in [Meta]'
         print 'section of the config file FrameworkConfig.cfg'
         print 'please refer to CaseDict.txt for supported cases'
+        print'===================================================================='
         exit()
 except IndexError:
     pass
