bad99146957ec64e96ce66031dc62d123f75e1bb
Unknown <jjjooonnn@gmail.com>
2018-06-23 16:08:48 +0200

    fix

--- MoonFrameWork/tests.py
+++ MoonFrameWork/tests.py
@@ -921,10 +921,9 @@ class TestTimeStampsDrift(TerminatingTest):
 
     def getvalue(self, string):
         result = ''
-        print len(string)
         for i in range(1, len(string)):
             result += string[i]
-            return float(result)
+        return float(result)
 
     def evaluate(self, lines, index):
         ignorefirst = True
