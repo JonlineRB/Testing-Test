9ac78bee25de4a2218188210a82da6af0cfc9dee
Unknown <jjjooonnn@gmail.com>
2018-06-28 20:35:51 +0200

    check

--- MoonFrameWork/ScapyTest.py
+++ MoonFrameWork/ScapyTest.py
@@ -2,7 +2,8 @@ from scapy.all import *
 
 
 def generatepcap():
-    pkts = sniff(prn=lambda x: x.sprintf("{IP:%IP.src% -> %IP.dst%\n}{Raw:%Raw.load%\n}"), count=1000)
+    # pkts = sniff(prn=lambda x: x.sprintf("{IP:%IP.src% -> %IP.dst%\n}{Raw:%Raw.load%\n}"), count=1000)
+    pkts = ICMP()
     wrpcap('tmp.pcap', pkts)
 
 def cleanpcap():
