94478f52e896e6e125f069c284ccee07710b80d9
Unknown <jjjooonnn@gmail.com>
2018-06-28 20:37:45 +0200

    check

--- MoonFrameWork/ScapyTest.py
+++ MoonFrameWork/ScapyTest.py
@@ -1,9 +1,11 @@
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
 
 def generatepcap():
-    # pkts = sniff(prn=lambda x: x.sprintf("{IP:%IP.src% -> %IP.dst%\n}{Raw:%Raw.load%\n}"), count=1000)
-    pkts = ICMP()
+    pkts = sniff(prn=lambda x: x.sprintf("{IP:%IP.src% -> %IP.dst%\n}{Raw:%Raw.load%\n}"), count=1000)
+    # pkts = ICMP()
     wrpcap('tmp.pcap', pkts)
 
 def cleanpcap():
