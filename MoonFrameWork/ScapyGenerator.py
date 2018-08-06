22f691e6442c4ac350cff361545b0d2efd73ce3f
Unknown <jjjooonnn@gmail.com>
2018-08-06 09:27:18 +0200

    muting console output during pcap generation

--- MoonFrameWork/ScapyGenerator.py
+++ MoonFrameWork/ScapyGenerator.py
@@ -1,13 +1,26 @@
-import os
+import os, sys
 # import logging
 
 # logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
+from contextlib import contextmanager
+
+
+def mute_console():
+    with open(os.devnull, "w") as devnull:
+        original_stdout = sys.stdout
+        sys.stdout = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = original_stdout
 
 
 def generatepcap():
-    pkts = sniff(prn=lambda x: x.sprintf("{IP:%IP.src% -> %IP.dst%\n}{Raw:%Raw.load%\n}"), count=2000)
-    wrpcap('tmp.pcap', pkts)
+    print 'Generating PCAP file'
+    with mute_console():
+        pkts = sniff(prn=lambda x: x.sprintf("{IP:%IP.src% -> %IP.dst%\n}{Raw:%Raw.load%\n}"), count=2000)
+        wrpcap('tmp.pcap', pkts)
 
 
 def cleanpcap():
