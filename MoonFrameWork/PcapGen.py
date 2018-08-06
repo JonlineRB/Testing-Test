70a034746b9843f4613054847d0f97794f58aacb
Unknown <jjjooonnn@gmail.com>
2018-08-06 09:46:54 +0200

    fix

--- MoonFrameWork/PcapGen.py
+++ MoonFrameWork/PcapGen.py
@@ -1,5 +1,5 @@
 from scapy.all import *
 
 print 'Generating PCAP file'
-    pkts = sniff(prn=lambda x: x.sprintf("{IP:%IP.src% -> %IP.dst%\n}{Raw:%Raw.load%\n}"), count=2000)
-    wrpcap('tmp.pcap', pkts)
+pkts = sniff(prn=lambda x: x.sprintf("{IP:%IP.src% -> %IP.dst%\n}{Raw:%Raw.load%\n}"), count=2000)
+wrpcap('tmp.pcap', pkts)
