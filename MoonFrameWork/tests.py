59b02d4089943660a656565f36f0545451a4295e
Unknown <jjjooonnn@gmail.com>
2018-09-16 16:17:33 +0200

    t

--- MoonFrameWork/tests.py
+++ MoonFrameWork/tests.py
@@ -832,7 +832,7 @@ class TestFiles(unittest.TestSuite):
         print "Testing the new search: "
         for subdir, dirs, files in os.walk(self.directory):
             for file in files:
-                print subdir + file
+                print os.path.relpath(file, parsedpath)
                 # self.addTest(TestFile(devicelist, path, file, parsedpath))
         print "Test concluded"
         # for filename in os.listdir(self.directory):
